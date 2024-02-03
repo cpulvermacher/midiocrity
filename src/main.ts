38be6e201061f38d7fbc6fc864666dc7167f12d0
Christian Pulvermacher <christian.pulvermacher@posteo.de>
2024-02-04 01:41:57 +0900

    gse d key for demo mode

--- src/main.ts
+++ src/main.ts
@@ -16,9 +16,20 @@ startMIDI({
   }
 });
 
-// play random note every second
-// setInterval(() => {
-//   const key = Math.floor(Math.random() * numKeys);
-//   piano.keyPressed(key, 0.5);
-//   setTimeout(() => piano.keyReleased(key), 10000);
-// }, 1000);
+// demo mode: play random notes
+let demoLoop: number | null = null;
+window.addEventListener('keydown', function (event) {
+  if (event.key === 'd') {
+    if (demoLoop !== null) {
+      clearInterval(demoLoop);
+      demoLoop = null;
+    } else {
+      demoLoop = setInterval(
+        () => {
+          const key = Math.floor(Math.random() * numKeys);
+          piano.keyPressed(key, 0.5);
+          setTimeout(() => piano.keyReleased(key), 3000);
+        }, 500);
+    }
+  }
+});

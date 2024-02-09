44fb5b206711104e3791bddc93e52e58aea5ef63
Christian Pulvermacher <christian.pulvermacher@posteo.de>
2024-02-09 23:21:36 +0900

    reduce nesting for midi.ts

--- src/midi.ts
+++ src/midi.ts
@@ -20,63 +20,65 @@ export function startMIDI(args: StartMidiArgs) {
 
     const activeChannels: ActiveChannels = {};
 
-    function onMIDISuccess(midiAccess: MIDIAccess) {
-        const inputs = midiAccess.inputs;
-
-        // Attach MIDI event "midimessage" to each input
-        inputs.forEach(function (input) {
-            console.log('Found MIDI input:', input.name, ", state: ", input.state);
-            input.onmidimessage = onMIDIMessage;
-        });
-
-        midiAccess.onstatechange = function (event: Event) {
-            const midiEvent = event as MIDIConnectionEvent;
-            if (midiEvent.port.type !== 'input') {
-                return;
-            }
-            const port = midiEvent.port as MIDIInput;
-            console.log('MIDI state change:', port.name, ", state: ", port.state);
-            if (port.state === 'connected') {
-                port.onmidimessage = onMIDIMessage;
-            } else {
-                port.onmidimessage = null;
-            }
-        };
-    }
-
-    function onMIDIMessage(message: Event) {
-        if (!("data" in message)) {
-            return;
-        }
-        // parse midi message
-        const data = message.data as Uint8Array;
-        const command = data[0] >> 4; //get first 4 bits
-        const channel = data[0] & 0x0F; // get last 4 bits
-        const note = data[1];
-        const velocity = data[2];
-
-        if (command === 9 && typeof note === 'number' && typeof velocity === 'number') {
-            activeChannels[channel] = true;
-            if (velocity === 0) {
-                args.onKeyReleased(note);
-            } else {
-                args.onKeyPressed(note, velocity);
-            }
-
-        } else if (command === 8 && typeof note === 'number') {
-            activeChannels[channel] = true;
-            args.onKeyReleased(note);
-        } else {
-            console.log("other midi msg", command, channel, note);
-        }
-    }
-
     navigator.requestMIDIAccess()
-        .then(onMIDISuccess, onMIDIFailure);
+        .then((midiAccess) => onSuccess(midiAccess, args, activeChannels))
+        .catch(onFailure);
 
     return { activeChannels };
 }
 
-function onMIDIFailure() {
+function onFailure() {
     console.log('Could not access your MIDI devices.');
+}
+
+function onSuccess(midiAccess: MIDIAccess, args: StartMidiArgs, activeChannels: ActiveChannels) {
+    const inputs = midiAccess.inputs;
+
+    const onMidiMessage = (message: Event) => processMessage(message, args, activeChannels);
+    // Attach MIDI event "midimessage" to each input
+    inputs.forEach(function (input) {
+        console.log('Found MIDI input:', input.name, ", state: ", input.state);
+        input.onmidimessage = onMidiMessage;
+    });
+
+    midiAccess.onstatechange = function (event: Event) {
+        const midiEvent = event as MIDIConnectionEvent;
+        if (midiEvent.port.type !== 'input') {
+            return;
+        }
+        const port = midiEvent.port as MIDIInput;
+        console.log('MIDI state change:', port.name, ", state: ", port.state);
+        if (port.state === 'connected') {
+            port.onmidimessage = onMidiMessage;
+        } else {
+            port.onmidimessage = null;
+        }
+    };
+}
+
+function processMessage(message: Event, args: StartMidiArgs, activeChannels: ActiveChannels) {
+    if (!("data" in message)) {
+        return;
+    }
+    // parse midi message
+    const data = message.data as Uint8Array;
+    const command = data[0] >> 4; //get first 4 bits
+    const channel = data[0] & 0x0F; // get last 4 bits
+    const note = data[1];
+    const velocity = data[2];
+
+    if (command === 9 && typeof note === 'number' && typeof velocity === 'number') {
+        activeChannels[channel] = true;
+        if (velocity === 0) {
+            args.onKeyReleased(note);
+        } else {
+            args.onKeyPressed(note, velocity);
+        }
+
+    } else if (command === 8 && typeof note === 'number') {
+        activeChannels[channel] = true;
+        args.onKeyReleased(note);
+    } else {
+        console.log("other midi msg", command, channel, note);
+    }
 }
